f268dc1f232b137eeba32e8be3a670ae71e9d177
Erki <alas.erki@gmail.com>
2024-06-17 11:13:17 +0300

    Initial commit

---
+++ src/main/java/com/example/lensRecommendationSystem/LensRecommendationSystemApplication.java
@@ -0,0 +1,14 @@
+package com.example.lensRecommendationSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LensRecommendationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LensRecommendationSystemApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/com/example/lensRecommendationSystem/LensRecommendationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lensRecommendationSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LensRecommendationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
